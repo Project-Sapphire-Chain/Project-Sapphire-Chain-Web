4d0a51b3bbe48ac886f173d3775d793fd75ba258
BenPrazak <78315067+BenPrazak@users.noreply.github.com>
2021-03-15 18:56:04 -0400

    Order feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
 import Product from './Products/Products';
 import Home from './Home/Home';
+import Orders from './Orders/Orders';
+import OrderDetail from './Orders/OrderDetails';
 import React from 'react';
 import './App.css';
 
@@ -30,6 +32,7 @@ function App() {
           <div className="header-links">
             <Link to="/">Home</Link>
             <Link to="/catalog">Catalog</Link>
+            <Link to="/orders">Orders</Link>
           </div>
         </header>
         <aside className="sidebar">
@@ -52,6 +55,12 @@ function App() {
             </Route>
             <Route path="/catalog">
               <Product />
+            </Route>
+            <Route path="/orders">
+              <Orders />
+            </Route>
+            <Route path="/order/:id">
+              <OrderDetail />
             </Route>
           </Switch>
         </main>
